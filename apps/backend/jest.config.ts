e797eeb61d4e660e6e341aebef0a6534a3d9d9b7
ebalo <ebalo55@users.noreply.github.com>
2023-06-04 19:05:01 +0200

    feat(9): added configuration for e2e testing inside backend

--- apps/backend/jest.config.ts
+++ apps/backend/jest.config.ts
@@ -3,6 +3,8 @@ export default {
 	displayName:          "backend",
 	preset:               "../../jest.preset.js",
 	testEnvironment:      "node",
+	testRegex:            ".*\\.(spec|e2e-spec)\\.ts$",
+	testMatch:            undefined,
 	transform:            {
 		"^.+\\.[tj]s$": [
 			"ts-jest",
