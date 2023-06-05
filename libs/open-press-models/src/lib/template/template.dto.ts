a8281df5070843c2d4532be5be70552fa4105f49
ebalo <ebalo55@users.noreply.github.com>
2023-06-05 09:45:01 +0200

    fix(9): missing template entity addition

--- libs/open-press-models/src/lib/template/template.dto.ts
+++ libs/open-press-models/src/lib/template/template.dto.ts
@@ -1,3 +1,7 @@
+import { Exclude } from "class-transformer";
+import { DateTime } from "luxon";
+import { MongoseId } from "@override/open-press-interfaces";
+
 export interface CreateTemplateDTO {
 	name: string;
 	html: string;
@@ -5,3 +9,27 @@ export interface CreateTemplateDTO {
 }
 
 export type UpdateTemplateDTO = Partial<CreateTemplateDTO>;
+
+export class TemplateEntity {
+	id!: string;
+	name!: string;
+	html!: string;
+	css!: string;
+
+	@Exclude()
+	created_at!: DateTime;
+
+	@Exclude()
+	updated_at!: DateTime;
+
+	constructor(template: Partial<TemplateEntity & MongoseId>) {
+		Object.assign(this, {
+			id:         template._id?.toHexString(),
+			name:       template.name,
+			html:       template.html,
+			css:        template.css,
+			created_at: template.created_at,
+			updated_at: template.updated_at,
+		});
+	}
+}
