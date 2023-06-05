aad61415d779f0f75981e9011d07b517937917b1
ebalo <ebalo55@users.noreply.github.com>
2023-06-05 16:02:20 +0200

    feat(10): added global validation pipe

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,17 @@
-import { Logger } from "@nestjs/common";
+import { Logger, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(new ValidationPipe({
+		forbidNonWhitelisted: true,
+		forbidUnknownValues:  true,
+		stopAtFirstError:     true,
+		whitelist:            true,
+		transform:            true,
+	}));
 
 	const port = process.env.PORT || 3000;
 	await app.listen(port);
