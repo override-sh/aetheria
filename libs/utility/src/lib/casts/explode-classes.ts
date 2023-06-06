1788544ad37b5c5163f1d0e09113cb885d3ec0be
ebalo <ebalo55@users.noreply.github.com>
2023-06-06 07:47:05 +0200

    feat(10): dead or replaced utilities removal

--- libs/utility/src/lib/casts/explode-classes.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Explode classes into an array of classes.
- * @example
- * explodeClasses("foo bar baz"); // ["foo", "bar", "baz"]
- * @param {string} classes The classes to explode
- * @returns {string[]} The exploded classes as an array
- */
-export const explodeClasses = (classes: string): string[] => classes.split(" ");
--- libs/utility/src/lib/filters/is-string.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Check if value is string
- * @param value - value to check
- * @returns {boolean} - true if value is string
- */
-export const isString = (value: any) => typeof value === "string";
